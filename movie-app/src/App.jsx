5cb935f282ca845cc20871cf7b4cafeb9040f0a3
Shiva <bshivasaip@gmail.com>
2025-09-14 19:39:26 +0530

    initial commit

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";     
+import Home from "./pages/Home";
+
 
 function App() {
   return (
     <>
-      <MovieCard movie={{ title: "Interstellar", release_date: "2024" }} />
-      <MovieCard movie={{ title: "Interstellar", release_date: "2024" }} />
-      <MovieCard movie={{ title: "Interstellar", release_date: "2024" }} />
+      <Home />
     </>
   );
 }
---
+++ movie-app/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "Inception", release_date: "2010" },
+    { id: 2, title: "The Dark Knight", release_date: "2008" },
+    { id: 3, title: "Interstellar", release_date: "2014" },
+    ];
+    
+    const handleSearch = () => {}   
+
+  return (
+      <div className="home">
+          <form onSubmit={handleSearch} className="search-form">
+              <input type="text"
+                  placeholder="Search for movies..."
+                  className="search-input" />
+          </form>
+          <button type="submit" className="searrch button">
+            Search  
+          </button>
+      <div className="movie-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+export default Home
